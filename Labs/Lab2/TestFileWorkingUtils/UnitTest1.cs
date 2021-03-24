186dc740bfdb770cbc44abba0a8521a85082c96f
VsIG <valentyn2202@gmail.com>
2021-03-24 12:40:41 +0200

    ✨: add first test

--- Labs/Lab2/TestFileWorkingUtils/UnitTest1.cs
+++ Labs/Lab2/TestFileWorkingUtils/UnitTest1.cs
@@ -1,15 +1,32 @@
-using System;
-using Xunit;
 using IIG.Core.FileWorkingUtils;
+using Xunit;
 
 namespace TestFileWorkingUtils
 {
+	/// <summary>
+	/// This class contain all tests for
+	/// library IIG.Core.FileWorkingUtils
+	/// </summary>
 	public class UnitTest1
 	{
+		/// <summary>
+		/// Test public property FilePath
+		/// Check if we get exact string as it is in constructor
+		/// </summary>
 		[Fact]
-		public void Test1()
+		public void FilePath_Get_ReturnsSameString()
 		{
-			FileWorker worker = new FileWorker("Labs/Lab2/Lab2/TempFile.txt");
+			// Arrange
+			const string FULL_PATH = "D:/ForStudy/Components-Of-" +
+				"Software-Engineering/Labs/Lab2/Lab2/TempFile.txt";
+			FileWorker worker = new(FULL_PATH);
+
+			// Act
+			string actual = "D:/ForStudy/Components-Of-" +
+				"Software-Engineering/Labs/Lab2/Lab2/TempFile.txt";
+
+			// Assert
+			Assert.Equal(actual, worker.FilePath);
 		}
 	}
 }
