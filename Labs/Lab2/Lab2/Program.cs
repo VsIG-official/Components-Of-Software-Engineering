ef8bc5149074a8a7bd2875e079b0117051ca2f5c
VsIG <valentyn2202@gmail.com>
2021-03-24 12:15:09 +0200

    ✨: add working path

--- Labs/Lab2/Lab2/Program.cs
+++ Labs/Lab2/Lab2/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using IIG.Core.FileWorkingUtils;
 
 namespace Lab2
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			Console.WriteLine("Hello World!");
+			FileWorker worker = new("D:/ForStudy/Components-Of-" +
+				"Software-Engineering/Labs/Lab2/Lab2/TempFile.txt");
+			Console.WriteLine(worker.FilePath);
 		}
 	}
 }
