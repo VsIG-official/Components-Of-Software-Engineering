55875d5945be3887ea81cbfe6a01c91938c47970
VsIG <valentyn2202@gmail.com>
2021-03-24 10:12:21 +0200

    ✨: update project

---
+++ Labs/Lab2/TestFileWorkingUtils/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using IIG.Core.FileWorkingUtils;
+
+namespace TestFileWorkingUtils
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			FileWorker worker = new FileWorker();
+		}
+	}
+}
