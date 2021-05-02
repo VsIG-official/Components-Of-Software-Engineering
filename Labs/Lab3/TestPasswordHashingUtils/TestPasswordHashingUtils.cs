55bb91df90dad20cddf45b687a18eeb4a051ae24
VsIG-official <valentyn2202@gmail.com>
2021-05-02 14:16:15 +0300

    ✨: add first classes and regions

--- Labs/Lab3/TestPasswordHashingUtils/TestPasswordHashingUtils.cs
+++ Labs/Lab3/TestPasswordHashingUtils/TestPasswordHashingUtils.cs
@@ -3,12 +3,45 @@ using Xunit;
 
 namespace TestPasswordHashingUtils
 {
+	/// <summary>
+	/// Class for testing Password Hashing Utils
+	/// </summary>
 	public class TestPasswordHashingUtils
 	{
-		[Fact]
-		public void Test1()
-		{
+		#region ExecutionRoutes
 
+		#region Init
+
+		/// <summary>
+		/// Class for testing Init method
+		/// </summary>
+		public class TestInit
+		{
+			[Fact]
+			public void Test1()
+			{
+
+			}
 		}
+
+		#endregion Init
+
+		#region GetHash
+
+		/// <summary>
+		/// Class for testing GetHash method
+		/// </summary>
+		public class TestGetHash
+		{
+			[Fact]
+			public void Test1()
+			{
+
+			}
+		}
+
+		#endregion GetHash
+
+		#endregion ExecutionRoutes
 	}
 }
