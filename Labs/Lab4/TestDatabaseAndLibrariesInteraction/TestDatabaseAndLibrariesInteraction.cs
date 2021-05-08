12e250c1c6332fe9d115c94d22ada25271498b3e
VsIG-official <valentyn2202@gmail.com>
2021-05-08 17:24:47 +0300

    ✨: add connection to DB

---
+++ Labs/Lab4/TestDatabaseAndLibrariesInteraction/TestDatabaseAndLibrariesInteraction.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using IIG.FileWorker;
+using IIG.DatabaseConnectionUtils;
+using IIG.CoSFE.DatabaseUtils;
+using IIG.PasswordHashingUtils;
+
+namespace TestDatabaseAndLibrariesInteraction
+{
+	public class TestDatabaseAndLibrariesInteraction
+	{
+		private const string Server = @"DESKTOP-GSU5395\IHBATESTEXPRESS";
+		private const string Database = @"IIG.CoSWE.AuthDB";
+		private const bool IsTrusted = false;
+		private const string Login = @"coswe";
+		private const string Password = @"L}EjpfCgru9X@GLj";
+		private const int ConnectionTimeout = 75;
+
+		StorageDatabaseUtils storageDatabase = new(Server, Database,
+			IsTrusted, Login, Password, ConnectionTimeout);
+
+
+	}
+}
