b735bf94b760cbdce9c82cb3a3a682849252e8e5
VsIG-official <valentyn2202@gmail.com>
2021-05-09 09:57:03 +0300

    🚧: if I use trusted connection, then something works

--- Labs/Lab4/Lab4/Program.cs
+++ Labs/Lab4/Lab4/Program.cs
@@ -9,27 +9,33 @@ namespace Lab4
 	class Program
 	{
 		private const string Server = @"VSIG-MACHINE";
-		private const string Database = @"IIG.CoSWE.AuthDB";
+		private const string AuthDatabase = @"IIG.CoSWE.AuthDB";
+		private const string StorageDatabase = @"IIG.CoSWE.StorageDB";
 		private const bool IsTrusted = false;
 		private const string Login = @"coswe";
 		private const string Password = @"L}EjpfCgru9X@GLj";
-		private const int ConnectionTimeout = 75;
+		private const int ConnectionTimeout = 20;
 
-		static StorageDatabaseUtils storageDatabase = new(Server, Database,
+		static StorageDatabaseUtils storageDatabase = new(Server, StorageDatabase,
+			IsTrusted, Login, Password, ConnectionTimeout);
+
+		static AuthDatabaseUtils authDatabase = new(Server, AuthDatabase,
 			IsTrusted, Login, Password, ConnectionTimeout);
 
 		static void Main()
 		{
-			byte[] array = new byte[100];
-			try
-			{
-				Console.WriteLine(storageDatabase.AddFile("SomeCoolName.txt", array));
-				// storageDatabase.ExecSql()
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e.Message);
-			}
+			Console.WriteLine(authDatabase.ConnectionString);
+			Console.WriteLine(authDatabase.AddCredentials("Cool guy", "qwerty"));
+			byte[] array = new byte[99];
+			//try
+			//{
+			Console.WriteLine(storageDatabase.AddFile("SomeCoolName2.txt", array));
+			//	// storageDatabase.ExecSql()
+			//}
+			//catch (Exception e)
+			//{
+			//	Console.WriteLine(e.Message);
+			//}
 		}
 	}
 }
